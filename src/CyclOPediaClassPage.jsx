4dad1ebc755eeb6a1ac00a72535b930a9e87a229
Dimitri Andre <dimitri@ymagyn.com>
2023-04-05 16:33:41 +0200

    api jsx

---
+++ src/CyclOPediaClassPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class CyclopediaClassPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instructor: undefined,
+      studentList: [],
+      studentCount: 0,
+      hideInstructor: false,
+    };
+  }
+  render() {}
+}
